04907377c10f421dbe5185145b8cf302734ddd64
BrundaMahesh <brunda.mahesh@ust.com>
2023-12-11 10:45:30 +0530

    added new class

--- RestExample/Program.cs
+++ RestExample/Program.cs
@@ -3,4 +3,28 @@
 string baseUrl = "https://reqres.in/api/";
 var client = new RestClient(baseUrl);
 
+//GET
 var getUserRequest = new RestRequest("users/2", Method.Get);
+var getUserResponse = client.Execute(getUserRequest);
+Console.WriteLine("GET Response: \n" + getUserResponse.Content);
+
+//POST
+var createUserRequest  = new RestRequest("users",Method.Post);
+createUserRequest.AddParameter("name", "John Doe");
+createUserRequest.AddParameter("job", "Software Developer");
+
+var createUserResponse  = client.Execute(createUserRequest);
+Console.WriteLine("POST Response: \n" + createUserResponse.Content);
+
+//PUT
+var  updateUserRequest = new RestRequest("users/2", Method.Put);
+updateUserRequest.AddParameter("name", "Updated John Doe");
+updateUserRequest.AddParameter("job", "Senior Software Developer");
+
+var updateUserResponse = client.Execute(updateUserRequest);
+Console.WriteLine("PUT Response: \n" + updateUserResponse.Content);
+
+//DELETE
+var deleteUserRequest = new RestRequest("users/2", Method.Delete);
+var deleteUserResponse = client.Execute(deleteUserRequest);
+Console.WriteLine("DELETE Response: \n" + deleteUserResponse.Content);
