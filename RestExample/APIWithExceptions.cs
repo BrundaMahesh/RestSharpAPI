b9aa22f61531c5ace636ed917b82893529ede42d
BrundaMahesh <brunda.mahesh@ust.com>
2023-12-12 12:10:45 +0530

    added new method

--- RestExample/APIWithExceptions.cs
+++ RestExample/APIWithExceptions.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace RestExample
     public class APIWithExceptions
     {
         string baseUrl = "https://reqres.in/api/";
+        /*
         public void GetSingleUser()
         { 
             var client = new RestClient(baseUrl);
@@ -38,6 +40,53 @@ namespace RestExample
             {
                 Console.WriteLine("Successful Response:");
                 Console.WriteLine(response.Content);
+            }
+
+        }
+        */
+
+
+        //JSon content check for null data
+        public void GetSingleUser()
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest("users/75", Method.Get);
+            var response = client.Execute(request);
+
+            if (!response.IsSuccessful)
+            {
+                if (IsJson(response.Content))
+                {
+                    try
+                    {
+                        var errorDetails = JsonConvert.DeserializeObject
+                            <ErrorResponse>(response.Content);
+                        if (errorDetails != null)
+                        {
+                            Console.WriteLine($"API Error: {errorDetails.Error}");
+                        }
+                    }
+                    catch (JsonException)
+                    {
+                        Console.WriteLine("Failed to deserialize error response");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Non-Json error response:{response.Content}");
+                }
+            }
+           static bool IsJson(string content)
+            {
+                try
+                {
+                    JToken.Parse(content);
+                    return true;
+                }
+                catch(ArgumentNullException)
+                {
+                    return false;
+                }
             }
 
         }
